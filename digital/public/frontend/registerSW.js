dec951ba65b4b1060ac9f5934a29e1f22d1b55f1
Sakshi-Greycube <sakshi@greycube.in>
2025-03-24 14:48:26 +0530

    feat: create pwa app

---
+++ digital/public/frontend/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/assets/project1/frontend/sw.js', { scope: '/assets/project1/frontend/' })})}
